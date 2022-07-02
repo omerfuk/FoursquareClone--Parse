b76174bef74d6012cdf69e9cf96d09e8302517ac
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-02 13:55:42 +0300

    Server connection with parse done

--- FoursquareClone- Parse/AppDelegate.swift
+++ FoursquareClone- Parse/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        let configuration = ParseClientConfiguration { ParseMutableClientConfiguration in
+            
+            ParseMutableClientConfiguration.applicationId = "yFKWv3a32maNxiJ1igifKdVKxPYPUW2LRmAqSzQH"
+            ParseMutableClientConfiguration.clientKey = "Awjo8aiLOlyol0AqVX9caNoj0vuEx1IIK0EWgRvR"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+            
+        }
+        
+        Parse.initialize(with: configuration)
+
         return true
     }
 
