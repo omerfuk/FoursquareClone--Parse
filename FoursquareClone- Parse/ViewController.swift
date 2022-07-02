f8b00d697b164bbaa90f619b259b66d942ce10f7
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-02 14:03:23 +0300

    fetching datas from Parse, filtering etc.

--- FoursquareClone- Parse/ViewController.swift
+++ FoursquareClone- Parse/ViewController.swift
@@ -13,9 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Apple"
-        parseObject["calories"] = 100
+    /*    let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Banana"
+        parseObject["calories"] = 150
         
         parseObject.saveInBackground { success, error in
             
@@ -27,6 +27,22 @@ class ViewController: UIViewController {
             }
         }
         
+     */
+        
+        //Fetching data from Parse
+        
+        let query = PFQuery(className: "Fruits")
+        //query.whereKey("name", equalTo: "Apple")
+        query.whereKey("calories", greaterThan: 120)
+        query.findObjectsInBackground { objects, error in
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else{
+                print(objects)
+            }
+        }
         
     }
 
