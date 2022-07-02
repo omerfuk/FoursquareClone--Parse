16a0e65628c423ceef10d69b6c267a20f7be7eeb
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-02 13:59:27 +0300

    Parse data saving,

--- FoursquareClone- Parse/ViewController.swift
+++ FoursquareClone- Parse/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        let parseObject = PFObject(className: "Fruits")
+        parseObject["name"] = "Apple"
+        parseObject["calories"] = 100
+        
+        parseObject.saveInBackground { success, error in
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            }
+            else{
+                print("uploaded")
+            }
+        }
         
         
     }
